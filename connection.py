806120490e3f6a1dfa51fe249c7118df50ef296c
Jayesh242663 <87438326+Jayesh242663@users.noreply.github.com>
2024-03-22 02:45:55 +0530

    connection error solved

--- connection.py
+++ connection.py
@@ -4,16 +4,19 @@ class Connection:
     _instance = None
 
     def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super().__new__(cls)
-            cls._instance.cnx = mysql.connector.connect(
-                user='root',
-                password='',
-                host='localhost',
-                port='3306',
-                database='bankingsys'
-            )
-        return cls._instance
+        try:
+            if not cls._instance:
+                cls._instance = super().__new__(cls)
+                cls._instance.cnx = mysql.connector.connect(
+                    user='root',
+                    password='',
+                    host='localhost',
+                    port='3306',
+                    database='bankingsys'
+                )
+            return cls._instance
+        except mysql.connector.Error as e:
+            print("Connection failed"+e)
 
     def get_connection(self):
         return self.cnx
