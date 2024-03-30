224d401bfa520d8db1c0bcc653abbfc9bdcfad96
Jayesh242663 <87438326+Jayesh242663@users.noreply.github.com>
2024-03-30 19:32:11 +0530

    transaction and history done

--- bankingsys.sql
+++ bankingsys.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: localhost    Database: varad
+-- Host: localhost    Database: bankingsys
 -- ------------------------------------------------------
--- Server version	8.0.36
+-- Server version	8.0.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -42,7 +42,7 @@ CREATE TABLE `acc_details` (
 
 LOCK TABLES `acc_details` WRITE;
 /*!40000 ALTER TABLE `acc_details` DISABLE KEYS */;
-INSERT INTO `acc_details` VALUES (1,'Varad','0003-12-24','Male','99648763215','abc@gmail.com','Dombivili','Savings',NULL),(2,'Varad','0003-12-24','Male','8745675432','abc@gmail.com','Dombivili','Current',NULL),(3,'Jayesh','0003-12-24','Male','7906467987','abc@gmail.com','Dombivili','Current',NULL),(4,'asd','1960-02-11','male','asd','asd','asd','Current Account',NULL),(5,'Mahesh','1965-02-15','male','9869567455','xyz@gmail.com','Kopar','Current Account','1000');
+INSERT INTO `acc_details` VALUES (1,'Varad','0003-12-24','Male','99648763215','abc@gmail.com','Dombivili','Savings','96850.59'),(2,'Varad','0003-12-24','Male','8745675432','abc@gmail.com','Dombivili','Current','18592.7'),(3,'Jayesh','0003-12-24','Male','7906467987','abc@gmail.com','Dombivili','Current',NULL),(4,'asd','1960-02-11','male','asd','asd','asd','Current Account',NULL),(5,'Mahesh','1965-02-15','male','9869567455','xyz@gmail.com','Kopar','Current Account','2954.21');
 /*!40000 ALTER TABLE `acc_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,13 +95,13 @@ INSERT INTO `login` VALUES (1,'a'),(2,'123'),(3,'abc'),(5,'a'),(6,'cvb');
 UNLOCK TABLES;
 
 --
--- Table structure for table `transaction_history`
+-- Table structure for table `transaction`
 --
 
-DROP TABLE IF EXISTS `transaction_history`;
+DROP TABLE IF EXISTS `transaction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `transaction_history` (
+CREATE TABLE `transaction` (
   `accno` int DEFAULT NULL,
   `sender_accno` int DEFAULT NULL,
   `name` varchar(45) DEFAULT NULL,
@@ -114,12 +114,41 @@ CREATE TABLE `transaction_history` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+INSERT INTO `transaction` VALUES (1,2,'vaarad',45,NULL,NULL,1),(2,2,'varad',75,NULL,NULL,2),(1,3,'jayesh',132,NULL,NULL,3);
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction_history`
+--
+
+DROP TABLE IF EXISTS `transaction_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction_history` (
+  `accno` int DEFAULT NULL,
+  `sender_accno` int DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `amount` varchar(20) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `transaction_id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`transaction_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `transaction_history`
 --
 
 LOCK TABLES `transaction_history` WRITE;
 /*!40000 ALTER TABLE `transaction_history` DISABLE KEYS */;
-INSERT INTO `transaction_history` VALUES (1,2,'vaarad',45,NULL,NULL,1),(2,2,'varad',75,NULL,NULL,2),(1,3,'jayesh',132,NULL,NULL,3);
+INSERT INTO `transaction_history` VALUES (1,2,'Varad','-20.5','2024-03-30','18:45:03',4),(1,2,'Varad','-25.22','2024-03-30','18:47:36',5),(2,1,'Varad','+25.22','2024-03-30','18:47:36',6),(1,2,'Varad','-45.25','2024-03-30','18:49:54',7),(2,1,'Varad','+45.25','2024-03-30','18:49:54',8),(5,2,'Varad','-25.23','2024-03-30','18:54:49',9),(2,5,'Varad','+25.23','2024-03-30','18:54:49',10),(5,1,'Varad','-20.56','2024-03-30','18:56:30',11),(1,5,'Varad','+20.56','2024-03-30','18:56:30',12),(1,5,'Mahesh','-2000.0','2024-03-30','18:57:03',13),(5,1,'Mahesh','+2000.0','2024-03-30','18:57:03',14),(1,2,'Varad','-500.0','2024-03-30','19:28:09',15),(2,1,'Varad','+500.0','2024-03-30','19:28:09',16);
 /*!40000 ALTER TABLE `transaction_history` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -132,4 +161,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-27 23:13:41
+-- Dump completed on 2024-03-30 19:31:40
