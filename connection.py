46cee0fda375ea5c3f96439877b8b211cab85aec
VaradBorole <varadborole@gmail.com>
2024-05-03 23:53:27 +0530

    grid applied

--- connection.py
+++ connection.py
@@ -10,10 +10,10 @@ class Connection:
                 cls._instance = super().__new__(cls)
                 cls._instance.cnx = mysql.connector.connect(
                     user='root',
-                    password='',
+                    password='9321985498',
                     host='localhost',
                     port='3306',
-                    database='bankingsys'
+                    database='banking_sys'
                 )
             return cls._instance
         except mysql.connector.Error as e:
